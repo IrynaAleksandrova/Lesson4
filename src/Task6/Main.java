cf8bc716d62e99ecbf970263361cbbe1a7bf6fc8
Iryna Lytkina <lytkina.iryna@gmail.com>
2022-10-02 18:46:06 +0300

    submit additional task6

--- src/Task6/Main.java
+++ src/Task6/Main.java
@@ -8,22 +8,32 @@ public class Main {
     //    Размер массива пусть задается с консоли и размер массива может быть больше 5 и меньше или равно 10.
     //    Если n не удовлетворяет условию - выведите сообщение об этом.
     //    Если пользователь ввёл не подходящее число, то программа должна просить пользователя повторить ввод.
-    //    ??? Создайте второй массив только из чётных элементов первого массива, если они там есть, и вывести его на экран.
+    //    Создайте второй массив только из чётных элементов первого массива, если они там есть, и вывести его на экран.
     public static void main(String[] args) {
-        int[] array;
         Scanner console = new Scanner(System.in);
-        System.out.println("Введите размер массива: ");
-        int N = console.nextInt();
-        array = new int[N];
-        if (N > 5 && N <= 10) {
-            for (int i = 0; i < array.length; i++) {
-                array[i] = (int) ((Math.random() * N) + 1);
+        int value;
+        do {
+            System.out.println("Введите число: ");
+            value = console.nextInt();
+        } while ((value < 5) || (value >= 10));
+        int[] massFirst = new int[value];
+        int counterValue = 0;
+        for (int j = 0; j < massFirst.length; j++) {
+            massFirst[j] = (int) ((Math.random() * 100)+1);
+            if ((massFirst[j] % 2) == 0) {
+                counterValue++;
             }
-            System.out.println(Arrays.toString(array));
-        } else {
-            System.out.println("Введите другой размер массива, согласно условию: ");
-            console.nextInt();
         }
+        System.out.println(Arrays.toString(massFirst));
+        int[] massSecond = new int[counterValue];
+        int i = 0;
+        for (int j = 0; j < massFirst.length; j++) {
+            if ((massFirst[j] % 2) == 0) {
+                massSecond[i] = massFirst[j];
+                i++;
+            }
+        }
+        System.out.println(Arrays.toString(massSecond));
         console.close();
     }
 }
